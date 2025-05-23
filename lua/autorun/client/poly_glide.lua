8bea1a52b3179cf43cde067fc1befe562172be1b
Valera <108022376+kekobka@users.noreply.github.com>
2025-05-23 10:59:01 +0300

    set cl_interp in vehicle

--- lua/autorun/client/poly_glide.lua
+++ lua/autorun/client/poly_glide.lua
@@ -4,3 +4,13 @@ list.Set("GlideCategories", "Poly - Sedan", {name = "Poly - Sedan", icon = "glid
 list.Set("GlideCategories", "Poly - Sport", {name = "Poly - Sport", icon = "glide/icons/car.png"})
 list.Set("GlideCategories", "Poly - Suv", {name = "Poly - Suv", icon = "glide/icons/car.png"})
 list.Set("GlideCategories", "Poly - Truck", {name = "Poly - Truck", icon = "glide/icons/car.png"})
+
+
+local cl_interp = GetConVar("cl_interp"):GetFloat()
+hook.Add( "Glide_OnLocalEnterVehicle", "Poly.Interp", function( vehicle, seatIndex )
+    RunConsoleCommand("cl_interp", 0)
+end )
+
+hook.Add( "Glide_OnLocalExitVehicle", "Poly.Interp", function()
+    RunConsoleCommand("cl_interp", cl_interp)
+end )
