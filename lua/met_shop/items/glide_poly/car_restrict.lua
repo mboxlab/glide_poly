de5756e2a362a0129473e1a686bec976142cc439
Valera <108022376+kekobka@users.noreply.github.com>
2025-08-11 08:55:07 -0700

    superuser can ignoring shop restrict

--- lua/met_shop/items/glide_poly/car_restrict.lua
+++ lua/met_shop/items/glide_poly/car_restrict.lua
@@ -1,6 +1,6 @@
 hook.Add("PlayerSpawnSENT", "MetShop.Restrict", function(ply, class)
 	if MetShop.Items[class] then
-		if not MetShop.HasItem(ply, class) then
+		if not MetShop.HasItem(ply, class) and not ply:IsSuperAdmin() then
             sdk.notifyLang(ply, "Эту машину можно приобрести в F2 магазине", 1, 3, {})
 			return false
 		end
