d3a604c66c53c3aaaa69523649d864d263e098ec
Valera <108022376+kekobka@users.noreply.github.com>
2025-08-11 08:46:35 -0700

    f3 -> f2 shop

--- lua/met_shop/items/glide_poly/car_restrict.lua
+++ lua/met_shop/items/glide_poly/car_restrict.lua
@@ -1,7 +1,7 @@
 hook.Add("PlayerSpawnSENT", "MetShop.Restrict", function(ply, class)
 	if MetShop.Items[class] then
 		if not MetShop.HasItem(ply, class) then
-            sdk.notifyLang(ply, "Эту машину можно приобрести в F3 магазине", 1, 3, {})
+            sdk.notifyLang(ply, "Эту машину можно приобрести в F2 магазине", 1, 3, {})
 			return false
 		end
 	end
