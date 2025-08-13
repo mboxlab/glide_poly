255ae05be5cc70113e429f7c057a09f0b672906c
Valera <108022376+kekobka@users.noreply.github.com>
2025-08-13 23:15:29 +0700

    del shop

--- lua/met_shop/items/glide_poly/car_restrict.lua
+++
@@ -1,8 +0,0 @@
-hook.Add("PlayerSpawnSENT", "MetShop.Restrict", function(ply, class)
-	if MetShop.Items[class] then
-		if not MetShop.HasItem(ply, class) and not ply:IsSuperAdmin() then
-            sdk.notifyLang(ply, "Эту машину можно приобрести в F2 магазине", 1, 3, {})
-			return false
-		end
-	end
-end, -1)
